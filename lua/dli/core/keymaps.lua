5ea2e7962e624b4ceb2fd6280a4abbb643d2d5a3
Dennis Li <dennisli@denniss-air.lan>
2024-08-18 23:27:19 -0700

    Updated keymaps.

--- lua/dli/core/keymaps.lua
+++ lua/dli/core/keymaps.lua
@@ -22,3 +22,12 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<A-Up>", ":m -2 <Enter>", { desc = "Moves current line up" }) -- moves line up
+keymap.set("n", "<A-Down>", ":m +1 <Enter>", { desc = "Moves current line down" }) -- moves line down
+
+keymap.set("n", "<S-Up>", "yyp", { desc = "Duplicates the current line" }) -- duplicates the curent line
+keymap.set("n", "<S-Down>", "yyp", { desc = "Duplicates the current line" }) -- duplicates the current line
+
+keymap.set("v", "<S-Up>", "VyP", { desc = "Duplicates the current line" }) -- duplicates the curent line
+keymap.set("v", "<S-Down>", "VyP", { desc = "Duplicates the current line" }) -- duplicates the current line
--- lua/dli/plugins/colorscheme.lua
+++ lua/dli/plugins/colorscheme.lua
@@ -61,7 +61,7 @@ return {
     local border = "#547998"
 
     require("nightfox").setup({
-      style = "Nordfox",
+      style = "nightfox",
       options = {
         -- Compiled file's destination location
         compile_path = vim.fn.stdpath("cache") .. "/nightfox",
@@ -80,7 +80,7 @@ return {
           },
         },
         styles = { -- Style to be applied to different syntax groups
-          comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+          comments = "bold", -- Value is any valid attr-list value `:help attr-list`
           conditionals = "NONE",
           constants = "NONE",
           functions = "NONE",
@@ -110,22 +110,55 @@ return {
 }
 
 -- return {
---   "rmehri01/onenord.nvim",
+--   "gbprod/nord.nvim",
 --   priority = 1000,
 --   config = function()
---     local transparent = false -- set to true if you would like to enable transparency
+--     -- local transparent = false -- set to true if you would like to enable transparency
 --
---     local bg = "#011628"
---     local bg_dark = "#011423"
---     local bg_highlight = "#143652"
---     local bg_search = "#0A64AC"
---     local bg_visual = "#275378"
---     local fg = "#CBE0F0"
---     local fg_dark = "#B4D0E9"
---     local fg_gutter = "#627E97"
---     local border = "#547998"
+--     require("nord").setup({})
+--     vim.cmd("colorscheme nord")
+--   end,
+-- }
 --
---     require("onenord").setup({})
---     vim.cmd("colorscheme onenord")
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   priority = 1000,
+--   config = function()
+--     -- local transparent = false -- set to true if you would like to enable transparency
+--
+--     require("solarized-osaka").setup({
+--       -- your configuration comes here
+--       -- or leave it empty to use the default settings
+--       transparent = true, -- Enable this to disable setting the background color
+--       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+--       styles = {
+--         -- Style to be applied to different syntax groups
+--         -- Value is any valid attr-list value for `:help nvim_set_hl`
+--         comments = { italic = true },
+--         keywords = { italic = true },
+--         functions = {},
+--         variables = {},
+--         -- Background styles. Can be "dark", "transparent" or "normal"
+--         sidebars = "dark", -- style for sidebars, see below
+--         floats = "dark", -- style for floating windows
+--       },
+--       sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+--       day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+--       dim_inactive = false, -- dims inactive windows
+--       lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+--
+--       --- You can override specific color groups to use other groups or a hex color
+--       --- function will be called with a ColorScheme table
+--       ---@param colors ColorScheme
+--       on_colors = function(colors) end,
+--
+--       --- You can override specific highlights to use other groups or a hex color
+--       --- function will be called with a Highlights and ColorScheme table
+--       ---@param highlights Highlights
+--       ---@param colors ColorScheme
+--       on_highlights = function(highlights, colors) end,
+--     })
+--     vim.cmd("colorscheme solarized-osaka")
 --   end,
 -- }
